3021469f2059ca7d51ee1266044220005d360859
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2023-03-30 16:49:16 +0530

    Added file

---
+++ C++/Trees/Count leaf nodes.cpp
@@ -0,0 +1,52 @@
+/***********
+Code : Count leaf nodes
+
+Given a generic tree, count and return the number of leaf nodes present in the given tree.
+Input format :
+The first line of input contains data of the nodes of the tree in level order form. The order is: data for root node, number of children to root node, data of each of child nodes and so on and so forth for each node. The data of the nodes of the tree is separated by space.  
+Output Format :
+The first and only line of output prints the count of leaf nodes present in the given tree.
+Constraints:
+Time Limit: 1 sec
+Sample Input 1 :
+10 3 20 30 40 2 40 50 0 0 0 0 
+Sample Output 1 :
+4
+**************/
+
+/************************************************************
+ 
+    Following is the structure for the TreeNode class
+
+    template <typename T>
+    class TreeNode {
+     public:
+        T data;
+        vector<TreeNode<T>*> children;
+    
+        TreeNode(T data) {
+            this->data = data;
+        }
+    
+        ~TreeNode() {
+            for (int i = 0; i < children.size(); i++) {
+                delete children[i];
+            }
+        }
+    };
+
+************************************************************/
+
+int getLeafNodeCount(TreeNode<int>* root) {
+    if(!root)
+       return 0;
+
+    if(!root->children.size())
+       return 1;
+
+    int leafCount=0;
+    for(int i=0; i<root->children.size(); i++) {
+           leafCount += getLeafNodeCount(root->children[i]);
+    }
+    return leafCount;
+}
