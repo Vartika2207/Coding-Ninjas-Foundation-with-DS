b28a9cda23de79f61d43afdf95973fcc047889b2
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2023-04-02 17:05:54 +0530

    Added file

---
+++ C++/Trees/Structurally identical.cpp
@@ -0,0 +1,65 @@
+/*****************
+Structurally identical
+
+Given two generic trees, return true if they are structurally identical. Otherwise return false.
+Structural Identical
+If the two given trees are made of nodes with the same values and the nodes are arranged in the same way, then the trees are called identical.  
+Input format :
+The first line of input contains data of the nodes of the first tree in level order form. The order is: data for root node, number of children to root node, data of each of child nodes and so on and so forth for each node. The data of the nodes of the tree is separated by space.  
+The following line of input contains data of the nodes of the second tree in level order form. The order is: data for root node, number of children to root node, data of each of child nodes and so on and so forth for each node. The data of the nodes of the tree is separated by space.
+Output format :
+The first and only line of output contains true, if the given trees are structurally identical and false, otherwise.
+Constraints:
+Time Limit: 1 sec
+Sample Input 1 :
+10 3 20 30 40 2 40 50 0 0 0 0 
+10 3 20 30 40 2 40 50 0 0 0 0
+Sample Output 1 :
+true
+Sample Input 2 :
+10 3 20 30 40 2 40 50 0 0 0 0 
+10 3 2 30 40 2 40 50 0 0 0 0
+Sample Output 2:
+false
+
+*********************/
+
+/************************************************************
+ 
+    Following is the structure for the TreeNode class
+
+    template <typename T>
+    class TreeNode {
+     public:
+        T data;
+        vector<TreeNode<T>*> children;
+    
+        TreeNode(T data) {
+            this->data = data;
+        }
+    
+        ~TreeNode() {
+            for (int i = 0; i < children.size(); i++) {
+                delete children[i];
+            }
+        }
+    };
+
+************************************************************/
+
+bool areIdentical(TreeNode<int> *root1, TreeNode<int> * root2) {
+    if(!root1 || !root2)
+       return true;
+    if(root1->data != root2->data)
+       return false;
+
+    if(root1->children.size() != root2->children.size()) 
+       return false;
+    
+    for(int i=0; i<root1->children.size(); i++) {
+        bool ans = areIdentical(root1->children[i], root2->children[i]);
+        if(!ans)
+           return false;
+    }
+    return true;
+}
