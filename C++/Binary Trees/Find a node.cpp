6125a8a707a684f8b981bedd85f55d67821cbe10
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2023-04-08 17:34:47 +0530

    Added file

---
+++ C++/Binary Trees/Find a node.cpp
@@ -0,0 +1,71 @@
+/**********
+Code : Find a node
+
+For a given Binary Tree of type integer and a number X, find whether a node exists in the tree with data X or not.
+ Input Format:
+The first line of each test case contains elements of the first tree in the level order form. The line consists of values of nodes separated by a single space. In case a node is null, we take -1 in its place.
+
+The second line of each test case contains the node value you have to find.
+
+Note:
+The above format was just to provide clarity on how the input is formed for a given tree.
+The sequence will be put together in a single line separated by a single space. Hence, for the above-depicted tree, the input will be given as:
+
+1 2 3 4 -1 5 6 -1 7 -1 -1 -1 -1 -1 -1
+Output Format:
+The only line of output prints 'true' or 'false'.
+
+The output of each test case should be printed in a separate line.
+Note:
+You are not required to print anything explicitly. It has already been taken care of.
+Constraints:
+1 <= N <= 10^5
+
+Where N is the total number of nodes in the binary tree.
+
+Time Limit: 1 sec.
+Sample Input 1:
+8 3 10 1 6 -1 14 -1 -1 4 7 13 -1 -1 -1 -1 -1 -1 -1
+7
+Sample Output 1:
+true
+Explanation For Output 1:
+Clearly, we can see that 7 is present in the tree. So, the output will be true.
+Sample Input 2:
+2 3 4 -1 -1 -1 -1
+10
+Sample Output 2:
+false
+
+**********/
+
+/************************************************************
+
+    Following is the Binary Tree node structure
+
+    template <typename T>
+    class BinaryTreeNode
+    {
+        public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data)
+        {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+bool isNodePresent(BinaryTreeNode<int> *root, int x) {
+    if(root==NULL) return false;
+    if(root->data==x) return true;
+    
+    bool leftSearch=isNodePresent(root->left,x);
+    bool rightSearch=isNodePresent(root->right,x);
+    return leftSearch||rightSearch;
+}
